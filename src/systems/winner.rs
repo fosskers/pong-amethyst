b3df9303837c99dca502ab49f82457e317611a9f
Colin Woodbury <colin@fosskers.ca>
2020-07-07 12:16:23 -0700

    Score updates

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -1,17 +1,47 @@
 use amethyst::core::Transform;
-use amethyst::ecs::{Join, System, WriteStorage};
-use pong::{Ball, ARENA_WIDTH};
+use amethyst::ecs::{Join, ReadExpect, System, Write, WriteStorage};
+use amethyst::ui::UiText;
+use pong::{Ball, ScoreBoard, ScoreText, ARENA_WIDTH};
 
 pub struct WinnerSystem;
 
 impl<'s> System<'s> for WinnerSystem {
-    type SystemData = (WriteStorage<'s, Ball>, WriteStorage<'s, Transform>);
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        WriteStorage<'s, Transform>,
+        WriteStorage<'s, UiText>,
+        Write<'s, ScoreBoard>,
+        ReadExpect<'s, ScoreText>,
+    );
 
-    fn run(&mut self, (mut balls, mut locals): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut balls, mut locals, mut ui_text, mut scores, score_text): Self::SystemData,
+    ) {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
-            if ball_x <= ball.radius || ball_x >= ARENA_WIDTH - ball.radius {
+            let did_hit = if ball_x <= ball.radius {
+                scores.score_right = (scores.score_right + 1).min(999);
+
+                if let Some(text) = ui_text.get_mut(score_text.p2_score) {
+                    text.text = scores.score_right.to_string();
+                }
+
+                true
+            } else if ball_x >= ARENA_WIDTH - ball.radius {
+                scores.score_left = (scores.score_left + 1).min(999);
+
+                if let Some(text) = ui_text.get_mut(score_text.p1_score) {
+                    text.text = scores.score_left.to_string();
+                }
+
+                true
+            } else {
+                false
+            };
+
+            if did_hit {
                 ball.velocity[0] *= -1.0;
                 transform.set_translation_x(ARENA_WIDTH / 2.0);
             }
