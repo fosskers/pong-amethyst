d61c9aab4edd45499dbbfe25b672bdfb8f985d62
Colin Woodbury <colin@fosskers.ca>
2020-07-07 11:23:31 -0700

    Refactor

--- src/systems/winner.rs
+++ src/systems/winner.rs
@@ -11,17 +11,7 @@ impl<'s> System<'s> for WinnerSystem {
         for (ball, transform) in (&mut balls, &mut locals).join() {
             let ball_x = transform.translation().x;
 
-            let did_hit = if ball_x <= ball.radius {
-                println!("Player 2 scores!");
-                true
-            } else if ball_x >= ARENA_WIDTH - ball.radius {
-                println!("Player 1 scores!");
-                true
-            } else {
-                false
-            };
-
-            if did_hit {
+            if ball_x <= ball.radius || ball_x >= ARENA_WIDTH - ball.radius {
                 ball.velocity[0] *= -1.0;
                 transform.set_translation_x(ARENA_WIDTH / 2.0);
             }
