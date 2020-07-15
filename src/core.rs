753a19bee24b61543b7d6d1597f71d269f41c105
Colin Woodbury <colin@fosskers.ca>
2020-07-15 15:07:01 -0700

    Slow the ball down, god

--- src/core.rs
+++ src/core.rs
@@ -6,7 +6,7 @@ pub const ARENA_WIDTH: f32 = 100.0;
 pub const PADDLE_HEIGHT: f32 = 16.0;
 pub const PADDLE_WIDTH: f32 = 4.0;
 
-pub const BALL_VELOCITY_X: f32 = 75.0;
+pub const BALL_VELOCITY_X: f32 = 65.0;
 pub const BALL_VELOCITY_Y: f32 = 50.0;
 pub const BALL_RADIUS: f32 = 2.0;
 
