6ca50069b17a49a92544839828d26826f8c65f6d
Colin Woodbury <colin@fosskers.ca>
2020-07-17 12:17:20 -0700

    Avoid updating the FPS counter every frame
    Doing it only twice per second is enough.

--- src/systems/fps.rs
+++ src/systems/fps.rs
@@ -1,5 +1,6 @@
 use crate::core::FPS;
-use amethyst::ecs::{ReadExpect, System, WriteStorage};
+use amethyst::core::timing::Time;
+use amethyst::ecs::{Read, ReadExpect, System, WriteStorage};
 use amethyst::ui::UiText;
 use amethyst::utils::fps_counter::FpsCounter;
 
@@ -10,11 +11,14 @@ impl<'s> System<'s> for FpsSystem {
         WriteStorage<'s, UiText>,
         ReadExpect<'s, FPS>,
         ReadExpect<'s, FpsCounter>,
+        Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut ui_text, fps, fps_counter): Self::SystemData) {
-        if let Some(text) = ui_text.get_mut(fps.0) {
-            text.text = format!("{:.0}", fps_counter.sampled_fps());
+    fn run(&mut self, (mut ui_text, fps, fps_counter, time): Self::SystemData) {
+        if time.frame_number() % 30 == 0 {
+            if let Some(text) = ui_text.get_mut(fps.0) {
+                text.text = format!("{:.0}", fps_counter.sampled_fps());
+            }
         }
     }
 }
